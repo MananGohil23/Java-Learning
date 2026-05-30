3ea3a7545912bd856a7fabb5a3c88de2b29fb5fe
Manan Gohil <manangohil2357@gmail.com>
2026-05-30 21:01:57 +0530

    -

--- string_reverser.java
+++ string_reverser.java
@@ -1,6 +1,9 @@
+import java.util.*;
 class revString{
+    Scanner sc = new Scanner(System.in);
     revString(){
-        String str = "Hello World";
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
         String rev = "";
         for(int i=str.length()-1; i>=0; i--)
         {
